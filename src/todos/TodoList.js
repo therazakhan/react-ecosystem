c973e1f795c2dc4907b7f0e56feb3f179eb840d8
Raza Khan <raza.khan@outlook.com>
2020-08-19 21:20:22 -0400

    add css files to components and styling

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
+import './TodoList.css';
+
 const TodoList = ({todos}) => (
     <div className="list-wrapper">
         {todos.map(todo => <TodoListItem todo={todo}/>)}
