507090a9436bd181df991a5a13b05622fec06945
Raza Khan <raza.khan@outlook.com>
2020-08-24 01:40:15 -0400

    use reselect npm
    using reselect differentiat the completed, incomplete todos and bind them with todoList component without major changes

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import TodoListItem from './TodoListItem';
 import NewTodoForm from './NewTodoForm';
-import { getTodos, getTodosLoading } from './selectors';
+import { getTodos, getTodosLoading, getCompleteTodos, getIncompleteTodos } from './selectors';
 import { loadTodos, removeTodoRequest, markTodoAsCompleteRequest } from './thunks';
 import './TodoList.css';
 
-const TodoList = ({ todos, isLoading, onRemovePressed, onCompletePressed, startLoadingTodos }) => {
+const TodoList = ({ completeTodos, inCompleteTodos, isLoading, onRemovePressed, onCompletePressed, startLoadingTodos }) => {
     useEffect(() => {
         startLoadingTodos()
     }, []);
@@ -14,7 +14,15 @@ const TodoList = ({ todos, isLoading, onRemovePressed, onCompletePressed, startL
     const content = (
         <div className="list-wrapper">
             <NewTodoForm />
-            {todos.map(todo => <TodoListItem
+            <h3>Incomplete:</h3>
+            {inCompleteTodos.map(todo => <TodoListItem
+                key={Math.random()}
+                todo={todo}
+                onRemovePressed={onRemovePressed}
+                onCompletePressed={onCompletePressed}
+            />)}
+            <h3>Completed:</h3>
+            {completeTodos.map(todo => <TodoListItem
                 key={Math.random()}
                 todo={todo}
                 onRemovePressed={onRemovePressed}
@@ -26,6 +34,8 @@ const TodoList = ({ todos, isLoading, onRemovePressed, onCompletePressed, startL
 const mapStateToProps = state => ({
     isLoading: getTodosLoading(state),
     todos: getTodos(state),
+    completeTodos: getCompleteTodos(state),
+    inCompleteTodos: getIncompleteTodos(state)
 });
 
 const mapDispatchToProps = dispatch => ({
